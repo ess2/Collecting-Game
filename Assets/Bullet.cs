fbf6f1980cb4da26576fce0cf9752bdce811a0ad
Egberto Santana <ess2@cin.ufpe.br>
2018-10-19 11:51:11 -0300

    Adição de novos prefabs e personagem consegue atirar.

---
+++ Assets/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public float speed = 20f;
+    public int damage = 40;
+    public Rigidbody2D rb;
+    public GameObject impactEffect;
+
+    void Start () {
+        rb.velocity = transform.right * speed;
+	}
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        YellowEnemy enemy = hitInfo.GetComponent<YellowEnemy>();
+
+        if(enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+
+        Instantiate(impactEffect, transform.position, transform.rotation);
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/YellowEnemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YellowEnemy : MonoBehaviour {
+
+    public int health = 40;
+
+    public GameObject deathYellowEnemy;
+
+    public void TakeDamage (int damage)
+    {
+        health -= damage;
+
+        if(health <=0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Instantiate(deathYellowEnemy, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
