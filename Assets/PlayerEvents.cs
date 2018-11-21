74bb9421c8a0db4ba2878e02c734daa7443a62c2
Egberto Santana <ess2@cin.ufpe.br>
2018-11-21 11:29:48 -0300

    Adiciona menu de pausa e menu principal.

---
+++ Assets/PlayerEvents.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEvents : MonoBehaviour {
+
+    private GameMasterScript gm;
+    
+	void Start () {
+        gm = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<GameMasterScript>();
+	}
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Coin"))
+        {
+            print(collider.gameObject);
+            Destroy(collider.gameObject);
+            gm.points += 1;
+        }
+    }
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public GameObject PausedUI;
+
+    private bool paused = false;
+
+    void Start()
+    {
+        PausedUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Pause"))
+        {
+            paused = !paused;
+        }
+
+        if(paused)
+        {
+            PausedUI.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+        if(!paused)
+        {
+            PausedUI.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
+    public void Resume()
+    {
+        paused = false;
+    }
+
+    public void Restart()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+
+    }
+
+    public void MainMenu()
+    {
+        Application.LoadLevel(0);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
