2417127ce870372767854f84ad4ba2a1f9a0c818
matheusfelic <mbdf@cin.ufpe.br>
2018-12-04 18:45:33 -0300

    adicionando o main menu com música e transições

---
+++ Assets/Scripts/TransitionStartGame.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionStartGame : MonoBehaviour {
+
+    public string sceneName;
+    public Animator transitionAnim;
+    public Animator soundAnim;
+   
+
+    public void StartGame()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    private IEnumerator LoadScene()
+    {
+        transitionAnim.SetTrigger("end");
+        soundAnim.SetTrigger("fadeOut");
+        yield return new WaitForSeconds(4f);
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
